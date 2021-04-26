0a6ea7d9a6be7a4878e5c953ed02c0ba17db28c5
unknown <ibmjavanh12@iiht.tech>
2021-04-26 16:03:36 +0530

    POST request using axios

--- src/userform/userform.js
+++ src/userform/userform.js
@@ -1,29 +1,37 @@
 import { useState } from "react";
+import axios from "axios";
 
 function Userform(){    //component,controller
     const [userform,setUserform]=useState({//useState-hook-predefined function
         name:'Ram',
-        age:20
+        mobileNumber:1
     })
-    return(//view
+    const save=function (){
+        console.log(userform.name,userform.mobileNumber);
+        axios   //fetch
+        .post('http://localhost:8083/user',userform)
+        .then(function(response){
+            console.log(response);
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+    };
+    return(//view   //JSX
         <div>
-            <input value={userform.name} onChange={handleEventName}></input>
-            <input value={userform.age} onChange={handleEventAge} placeholder='Age'></input>
+            <input value={userform.name} name='name' onChange={updateState}></input>
+            <input value={userform.mobileNumber} name='mobileNumber' onChange={updateState} placeholder='Age'></input>
+            
             <button onClick={save}>Save</button>
         </div>
     )
-    function save(){
-        console.log(userform.name,userform.age);
-    }
-    function handleEventName(event){
-        setUserform({
-            name:event.target.value,
+  
+    function updateState(event){
+        setUserform(
+            Object.assign(userform,{
+            [event.target.name]:event.target.value
         })
-    }
-    function handleEventAge(event){
-        setUserform({
-            age:event.target.value,
-        })
+        );
     }
 }
 
